aa194414abe240109c21bcffee675647e668edc3
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-01-17 01:32:05 -0800

    try to hide and show only inline block for specific element

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
   $("#entry").submit(function(event){
     event.preventDefault();
     $("ul#book").append("<li>" + $("input#first-name").val() + " " + $("input#last-name").val() + " <span class='phone-number'>" + $("input#phone").val() + "</span>" + "</li>");
-    
-    $(".phone-number").hide();
+
+    $("ul#book").children("li").first().click(function(event){
+    alert($("this").children().show();
+    event.preventDefault();
+    //  $("this").children().show();
+    });
+
+    $$("ul#book").children("li").first()("this.phone-number").hide();
+  //  location.reload();
   });
 });
